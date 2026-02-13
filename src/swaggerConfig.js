a3fd4a65dce0ae3abd7a4ad08ed0cbdf5d70834e
Sandrine P <sandrine.pham.89@gmail.com>
2026-02-13 12:16:04 +0100

    Fix swagger assets for vercel

--- src/swaggerConfig.js
+++ src/swaggerConfig.js
@@ -2,9 +2,13 @@ const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const path = require("path");
 
-// Charger le fichier swagger.yaml depuis la racine
 const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
 
 module.exports = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  // IMPORTANT en serverless : serveFiles pour les assets (bundle.js, preset.js…)
+  app.use(
+    "/api-docs",
+    swaggerUi.serveFiles(swaggerDocument),
+    swaggerUi.setup(swaggerDocument)
+  );
 };
