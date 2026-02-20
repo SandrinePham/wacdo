ad73ca47a4fd1e50b2e26ea01f0503d51013de7b
Sandrine P <sandrine.pham.89@gmail.com>
2026-02-21 00:26:10 +0100

    Fix Swagger on Vercel using CDN assets

--- src/swaggerConfig.js
+++ src/swaggerConfig.js
@@ -1,21 +1,15 @@
 const path = require("path");
-const express = require("express");
 const YAML = require("yamljs");
-const swaggerUiDist = require("swagger-ui-dist");
 
 const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
 
 module.exports = (app) => {
-  // 1) Spec JSON (tu l’as déjà et ça marche)
+  // Exposer le spec (tu l'as déjà, on garde)
   app.get("/swagger.json", (req, res) => {
     res.json(swaggerDocument);
   });
 
-  // 2) Assets Swagger UI (JS/CSS) servis en statique
-  const swaggerUiPath = swaggerUiDist.getAbsoluteFSPath();
-  app.use("/api-docs", express.static(swaggerUiPath));
-
-  // 3) Page HTML Swagger UI qui pointe vers /swagger.json
+  // Swagger UI via CDN (aucun asset statique à servir côté Vercel)
   app.get("/api-docs", (req, res) => {
     res.type("html").send(`
 <!DOCTYPE html>
@@ -23,13 +17,16 @@ module.exports = (app) => {
 <head>
   <meta charset="UTF-8" />
   <title>Wacdo API Docs</title>
-  <link rel="stylesheet" href="/api-docs/swagger-ui.css" />
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+  <style>
+    body { margin: 0; }
+  </style>
 </head>
 <body>
   <div id="swagger-ui"></div>
 
-  <script src="/api-docs/swagger-ui-bundle.js"></script>
-  <script src="/api-docs/swagger-ui-standalone-preset.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js"></script>
   <script>
     window.onload = () => {
       SwaggerUIBundle({
@@ -48,6 +45,6 @@ module.exports = (app) => {
     `);
   });
 
-  // Support du slash final
+  // support du slash final
   app.get("/api-docs/", (req, res) => res.redirect("/api-docs"));
 };
