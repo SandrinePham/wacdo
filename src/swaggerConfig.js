3ab17bffa20a6861b52c3d394db4d133f2d779e0
Sandrine P <sandrine.pham.89@gmail.com>
2026-02-20 23:52:34 +0100

    vercel swagger

--- src/swaggerConfig.js
+++ src/swaggerConfig.js
@@ -2,9 +2,24 @@ const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const path = require("path");
 
-// Charger le fichier swagger.yaml depuis la racine
 const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
 
 module.exports = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  // 1) Exposer le spec en JSON (plus robuste en prod)
+  app.get("/swagger.json", (req, res) => {
+    res.json(swaggerDocument);
+  });
+
+  // 2) Servir les assets swagger-ui (JS/CSS)
+  app.use("/api-docs", swaggerUi.serve);
+
+  // 3) Servir la page Swagger UI en pointant vers /swagger.json
+  const setup = swaggerUi.setup(null, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  });
+
+  app.get("/api-docs", setup);
+  app.get("/api-docs/", setup);
 };
